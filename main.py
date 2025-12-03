832444df043463003859e500dda8c4fd0061fbf8
krithikag23 <113279833+krithikag23@users.noreply.github.com>
2025-12-03 22:29:39 +0530

    create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+import joblib
+import numpy as np
+from sklearn.datasets import load_breast_cancer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+import shap
+import os
