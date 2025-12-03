03fb201451ece3d14400557b9fa5048b494313a3
krithikag23 <113279833+krithikag23@users.noreply.github.com>
2025-12-03 22:31:35 +0530

    update

--- app_streamlit.py
+++ app_streamlit.py
@@ -5,3 +5,6 @@ from sklearn.datasets import load_breast_cancer
 import shap
 import matplotlib.pyplot as plt
 import pandas as pd
+
+st.set_page_config(layout="wide")
+st.title("XAI Demo — Breast Cancer (RandomForest + SHAP)")
