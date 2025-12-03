5b9d708159e29382d32530fc6c51d2b2eb4005ef
krithikag23 <113279833+krithikag23@users.noreply.github.com>
2025-12-03 22:30:02 +0530

    update

--- main.py
+++ main.py
@@ -6,3 +6,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 import shap
 import os
+
+MODEL_PATH = "model.joblib"
+EXPLAINER_PATH = "shap_explainer.joblib"
